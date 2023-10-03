df38a89f9a33c2f6ae86bf4cf08f28f5b01d929f
Jack Henza <jackhenza@gmail.com>
2023-10-03 09:48:31 -0400

    selects drivers and multiplies fares

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+    return arr.slice(0,2);
+}
+//console.log(returnFirstTwoDrivers(["kate","joe","bob"]))
+const returnLastTwoDrivers = (arr) => {
+    return arr.slice(-2);
+}
+//console.log(returnLastTwoDrivers(["kate","joe","bob","jenny","dave"]))
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+function createFareMultiplier(num){
+    return (fare)=>fare*num
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(names,selector){
+    return selector(names);
+}
